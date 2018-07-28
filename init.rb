4812f78c4ae999fb585433e783875da642489525
Igor Morse <igormorse@hotmail.com>
2018-07-28 01:45:26 -0300

    fix(init): Changed register name and basic info

--- init.rb
+++ init.rb
@@ -1,11 +1,11 @@
 # encoding: utf-8
 require_dependency 'sidebar_hook_listener'
 
-Redmine::Plugin.register :sidebar_hide do
+Redmine::Plugin.register :redmine_collapsable_sidebar do
   name 'Sidebar Hide Plugin'
   author 'Berk Demirkır'
   description 'This plugin provides ability to hide sidebar'
   version '0.0.8'
-  url 'https://github.com/bdemirkir/sidebar_hide'
+  url 'https://github.com/igormorse/redmine_collapsable_sidebar'
   author_url 'https://github.com/bdemirkir'
 end
